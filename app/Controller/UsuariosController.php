ebf7458eee9805aff30c4ba6907ea2e97e06e015
Anderson Possamai <anderson@possamai.net.br>
2013-07-05 00:33:33 -0300

    Início

---
+++ app/Controller/UsuariosController.php
@@ -0,0 +1,13 @@
+<?php
+class UsuariosController extends AppController {
+    var $name = 'Usuarios';
+    
+    function index() {
+        
+        $this->set('teste', 'testecontroller');
+        
+    }
+    
+    
+    
+}
---
+++ app/View/Usuarios/index.ctp
@@ -0,0 +1,8 @@
+<h1>Gerencie.se</h1>
+<h2>Lista de Usuários</h2>
+
+<?PHP
+
+print_r($teste); 
+
+?>
